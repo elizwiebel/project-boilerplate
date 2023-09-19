134b6f09e5cae5b2a06c1359ba424e01168bf882
Eli Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-09-19 16:36:57 +0200

    Fetch service functions for both async and then methods

---
+++ services/fetch-data-async-method.js
@@ -0,0 +1,19 @@
+const fetchDataAsyncMethod = async (url) => {
+  try {
+    const response = await fetch(url)
+
+    if (!response.ok) {
+      const message = `${response.status}`;
+      throw new Error(message);
+    }
+
+    const data = await response.json()
+    return data
+  }
+  catch (error) {
+    console.error(error)
+  }
+}
+
+// TEST CONFIG
+// fetchDataAsyncMethod("https://jsonplaceholder.typicode.com/todos/1").then(data => console.log("data", data))
---
+++ services/fetch-data-then-method.js
@@ -0,0 +1,18 @@
+const fetchDataThenMethod = (url) => {
+  return fetch(url)
+    .then(response => {
+
+      if (!response.ok) {
+        const message = `${response.status}`;
+        throw new Error(message);
+      }
+
+      return response.json()
+
+    }).catch((error) => {
+      console.error(error)
+    })
+}
+
+// TEST CONFIG
+// fetchDataThenMethod("https://jsonplaceholder.typicode.com/todos/1").then(data => console.log("data", data))
