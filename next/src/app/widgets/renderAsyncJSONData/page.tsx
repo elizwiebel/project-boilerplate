cf8c63b3e58c9a99b7b681cfec0727189c771df6
Eli Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-10-28 18:55:57 +0200

    Move react widget to next app

---
+++ next/src/app/widgets/renderAsyncJSONData/page.tsx
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+// import styles from './page.module.css'
+
+import RenderAsyncJSONData from './widget';
+
+export default function Widget() {
+    return <RenderAsyncJSONData />;
+}
---
+++ next/src/app/widgets/renderAsyncJSONData/widget.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+// import styles from './page.module.css'
+
+export default function RenderAsyncJSONData() {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    console.log('component renders');
+
+    useEffect(() => {
+        console.log('useEffect with no dependencies runs');
+
+        async function fetchData() {
+            console.log('fetching data starts');
+
+            try {
+                const response = await fetch('/datad.json');
+                const data = await response.json();
+
+                if (!response.ok) {
+                    const message = `${response.status}`;
+                    throw new Error(message);
+                }
+
+                setData(data);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+        <ul>
+            {data.map((item) => (
+                <li key={item._id}>{item.name}</li>
+            ))}
+        </ul>
+    );
+}
