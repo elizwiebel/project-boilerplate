d11f9da193fc565e39058699b71eadc750a13115
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-11-25 13:16:48 -0700

    Add new tab widget example

---
+++ widgets/tabs/scripts.js
@@ -0,0 +1,24 @@
+"use strict";
+;
+(() => {
+    const tabBtns = document.querySelectorAll(".tab-btn");
+    const tabContents = document.querySelectorAll(".tab-content");
+    let currentTab = 0;
+    for (let i = 0; i < tabBtns.length; i++) {
+        console.log("tabBtns", tabBtns);
+        const test = tabBtns[i];
+        test.addEventListener("click", (e) => btnClickHandler(e, i));
+    }
+    function btnClickHandler(e, i) {
+        console.log("click", e, i);
+        if (i === currentTab)
+            return;
+        if (i >= tabContents.length)
+            throw new Error(`Missing tab content element ${i}`);
+        tabBtns[currentTab].classList.toggle("--active");
+        tabBtns[i].classList.toggle("--active");
+        tabContents[currentTab].classList.toggle("--active");
+        tabContents[i].classList.toggle("--active");
+        currentTab = i;
+    }
+})();
