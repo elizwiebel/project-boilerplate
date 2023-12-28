6cf73282e7ce316404a0bda7525727add45dd0e0
Elijah Zwiebel <54692584+elizwiebel@users.noreply.github.com>
2023-12-28 01:35:06 +0100

    Updates to accordion transition

--- widgets/accordions/scripts.js
+++ widgets/accordions/scripts.js
@@ -2,10 +2,18 @@
 (() => {
     const accordionBtns = document.querySelectorAll(".accordion-btn");
     for (const btn of accordionBtns) {
+        const panel = btn.nextElementSibling;
+        if (panel.classList.contains("--open")) {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
         btn.addEventListener("click", (e) => {
             const panel = e.target.nextElementSibling;
-            if (!panel)
-                return;
+            if (!panel) return;
+            if (panel.style.maxHeight) {
+                panel.removeAttribute("style");
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
             e.target.classList.toggle("--open");
             panel.classList.toggle("--open");
         });
--- widgets/accordions/scripts.ts
+++ widgets/accordions/scripts.ts
@@ -2,10 +2,22 @@
     const accordionBtns = document.querySelectorAll(".accordion-btn")! as NodeListOf<HTMLElement>;
 
     for (const btn of accordionBtns) {
+        const panel = btn.nextElementSibling as HTMLElement;
+
+        if (panel.classList.contains("--open")) {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+
         btn.addEventListener("click", (e: Event): void => {
-            const panel = (e.target as HTMLElement).nextElementSibling;
+            const panel = (e.target as HTMLElement).nextElementSibling as HTMLElement;
 
             if (!panel) return;
+
+            if (panel.style.maxHeight) {
+                panel.removeAttribute("style");
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            }
 
             (e.target as HTMLElement).classList.toggle("--open");
             panel.classList.toggle("--open");
